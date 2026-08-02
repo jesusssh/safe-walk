68148a08173057ac2932a3064bd9fd3433c2afe2
asaf3135 <josue.campos2027@frech.superate.org>
2026-08-01 22:03:40 -0600

    avanze

---
+++ public/js/verificar-codigo.js
@@ -0,0 +1,17 @@
+const inputs =
+document.querySelectorAll(".codigo-input");
+
+inputs.forEach((input,index)=>{
+
+    input.addEventListener("input",()=>{
+
+        if(
+            input.value.length === 1 &&
+            index < inputs.length-1
+        ){
+            inputs[index+1].focus();
+        }
+
+    });
+
+});
